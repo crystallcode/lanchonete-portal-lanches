9122efe0467989a4321f2e9bb74b43a9b4975b2f
Crystal Code <hf444fv@gmail.com>
2024-09-03 14:19:02 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -319,7 +319,7 @@ function checkRestaurantOpen() {
     const data = new Date();
     const hora = data.getHours();
     const minutos = data.getMinutes();
-    return (hora > 18 || (hora === 18 && minutos >= 0)) && (hora < 23 || (hora === 23 && minutos === 0));
+    return (hora > 11 || (hora === 11 && minutos >= 0)) && (hora < 12 || (hora === 12 && minutos === 0));
 }
 
 const spanItem = document.getElementById("date-span");
